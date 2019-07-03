8398ce462d0b3c9f601e2861c991d349d9085380
ithingv <ithingv@naver.com>
2019-07-03 17:00:29 +0900

    Spinner Component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import Spinner from "./Spinner";
 import registerServiceWorker from './registerServiceWorker';
 import firebase from './firebase';
 import 'semantic-ui-css/semantic.min.css';
@@ -33,7 +34,7 @@ class Root extends React.Component {
   
     render(){
     
-    return (
+    return this.props.isLoading ? <Spinner /> : (
             <Switch>
                 <Route exact path="/" component={App}/>
                 <Route path="/login" component={Login}/>
@@ -43,7 +44,16 @@ class Root extends React.Component {
     )}
 };
 
-const RootWithAuth = withRouter(connect(null, { setUser })(Root));
+const mapStateFromProps = state => ({
+    isLoading: state.user.isLoading
+})
+
+const RootWithAuth = withRouter(
+    connect(
+        mapStateFromProps, 
+        { setUser }
+    )(Root)
+);
 
 ReactDOM.render(
 <Provider store={store}>
