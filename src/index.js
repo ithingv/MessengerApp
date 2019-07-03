f49db946956b5b092a944f54ac40b835e047a688
ithingv <ithingv@naver.com>
2019-07-03 15:19:57 +0900

    onAuthStateChanged to Listen for Login/Register

--- src/index.js
+++ src/index.js
@@ -4,22 +4,37 @@ import App from './components/App';
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import registerServiceWorker from './registerServiceWorker';
-
+import firebase from './firebase';
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route , withRouter} from 'react-router-dom';
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
- 
-        </Switch>
-    </Router>
-);
+class Root extends React.Component {
+    // run by based on our on our state changes as a listener
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                this.props.history.push('/'); // redirect them to home route
+            }
+        })
+    }
+  
+    render(){
+    
+    return (
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+    
+            </Switch>
+    )}
+};
 
+const RootWithAuth = withRouter(Root);
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+<RootWithAuth />
+</Router>, document.getElementById('root'));
 registerServiceWorker();
