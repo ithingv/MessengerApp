e67b1bf1eff8eddc557ac3c6c02883c01f7d8a13
ithingv <ithingv@naver.com>
2019-07-04 15:35:45 +0900

    Message state

--- src/components/App.js
+++ src/components/App.js
@@ -9,13 +9,21 @@ import MetaPanel from './MetaPanel/MetaPanel';
 
 import './App.css';
 
-const App = ({currentUser}) => (
+const App = ({currentUser, currentChannel}) => (
   <Grid columns="equal" className="app" style={{ background: '#eee'}}>
     <ColorPanel/>
-    <SidePanel currentUser ={currentUser}/>
+    <SidePanel 
+        key={currentUser && currentUser.uid}
+        currentUser ={currentUser}
+    
+    />
 
     <Grid.Column style={{ marginLeft: 320}}>
-      <Messages />
+      <Messages
+        key = {currentChannel && currentChannel.id}
+        currentChannel = {currentChannel}
+        currentUser = {currentUser}
+      />
     </Grid.Column>
     
     <Grid.Column width={4}>
@@ -28,7 +36,8 @@ const App = ({currentUser}) => (
 );
 
 const mapStateProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel
 })
 
 export default connect(mapStateProps)(App);
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import MessagesHeader from './MessagesHeader';
+import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
+
+import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
-import { Segment, Comment} from 'semantic-ui-react';
 
-class Messages extends React.Component{
-    render(){
-        return(
-            <React.Fragment>
-                <MessagesHeader />
-                
-                <Segment>
-                    <Comment.Group className="messages">
-                        {/* Messages */}
-                    </Comment.Group>
-                </Segment>
-                <MessageForm />
+class Messages extends React.Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
 
-            </React.Fragment>
-        )
-    }
+  render() {
+    const { messagesRef, channel, user } = this.state;
+
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Messages;
