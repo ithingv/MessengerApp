debabf51afcfc9ba2bd04d6ced24f57a31d8224f
ithingv <ithingv@naver.com>
2019-07-05 14:38:50 +0900

    Create Direct Message

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -116,7 +116,7 @@ class Channels extends React.Component{
 
         return(
             <React.Fragment>
-            <Menu.Menu style={{paddingBottom: '2em' }}>
+            <Menu.Menu className="menu" style={{paddingBottom: '2em' }}>
                 <Menu.Item>
                     <span>
                         <Icon name="exchange"/> CHANNELS
