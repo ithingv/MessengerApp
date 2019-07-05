bd196c649a8e203be1804a3bc8ec5cf69eb84538
ithingv <ithingv@naver.com>
2019-07-05 17:28:54 +0900

    Active panel

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { setCurrentChannel, setPrivateChannel } from '../../actions';
 class DirectMessages extends React.Component {
   state = {
+    activeChannel: '',
     user: this.props.currentUser,
     users: [],
     usersRef: firebase.database().ref("users"),
@@ -75,6 +76,7 @@ class DirectMessages extends React.Component {
       };
       this.props.setCurrentChannel(channelData);
       this.props.setPrivateChannel(true);
+      this.setActiveChannel(user.uid);
   }
 
   getChannelId = userId => {
@@ -84,8 +86,12 @@ class DirectMessages extends React.Component {
         // unique channel identifier for every direct messages channel
     }
 
+  setActiveChannel = userId =>{
+      this.setState({ activeChannel: userId})
+  }
+
   render() {
-    const { users } = this.state;
+    const { users, activeChannel } = this.state;
 
     return (
       <Menu.Menu className="menu">
@@ -98,6 +104,7 @@ class DirectMessages extends React.Component {
         {users.map(user => (
           <Menu.Item
             key={user.uid}
+            active={user.uid == activeChannel}
             onClick={() => this.changeChannel(user)}
             style={{ opacity: 0.7, fontStyle: "italic" }}
           >
